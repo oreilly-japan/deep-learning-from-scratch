0fe7fe33a99a223a31b851a82d9a9a1eeca063d3
Saitoh Koki <koki0702@gmail.com>
2016-07-16 15:21:11 +0900

    Update multi_layer_net.py

--- common/multi_layer_net.py
+++ common/multi_layer_net.py
@@ -154,7 +154,7 @@ class MultiLayerNet:
         # 設定
         grads = {}
         for idx in range(1, self.hidden_layer_num+2):
-            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW
+            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + self.weight_decay_lambda * self.layers['Affine' + str(idx)].W
             grads['b' + str(idx)] = self.layers['Affine' + str(idx)].db
 
         return grads
