bcc764a728e0c2b258ac868d587433534610447f
Saitoh Koki <koki0702@gmail.com>
2016-07-21 17:40:58 +0900

    Update nand_gate.py

--- ch02/nand_gate.py
+++ ch02/nand_gate.py
@@ -15,4 +15,4 @@ def NAND(x1, x2):
 if __name__ == '__main__':
     for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
         y = NAND(xs[0], xs[1])
-        print(str(xs) + " -> " + str(y)) # 0を出力
+        print(str(xs) + " -> " + str(y))
