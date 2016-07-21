9b822dab3dd1648e709a4c6d0f11279c4fd96c03
Saitoh Koki <koki0702@gmail.com>
2016-07-21 17:40:34 +0900

    Update and_gate.py

--- ch02/and_gate.py
+++ ch02/and_gate.py
@@ -15,4 +15,4 @@ def AND(x1, x2):
 if __name__ == '__main__':
     for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
         y = AND(xs[0], xs[1])
-        print(str(xs) + " -> " + str(y)) # 0を出力
+        print(str(xs) + " -> " + str(y))
