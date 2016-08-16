5b2356611192992bc48bd8e6fbf9c59192767d44
Saitoh Koki <koki0702@gmail.com>
2016-08-16 17:47:16 +0900

    Modify:np.sum()

--- ch02/nand_gate.py
+++ ch02/nand_gate.py
@@ -6,7 +6,7 @@ def NAND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([-0.5, -0.5])
     b = 0.7
-    tmp = (w*x).sum() + b
+    tmp = np.sum(w*x) + b
     if tmp <= 0:
         return 0
     else:
--- ch02/or_gate.py
+++ ch02/or_gate.py
@@ -6,7 +6,7 @@ def OR(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
     b = -0.2
-    tmp = (w*x).sum() + b
+    tmp = np.sum(w*x) + b
     if tmp <= 0:
         return 0
     else:
