471ff64c25d27eaad58d8b5a9e787249db974d44
Saitoh Koki <koki0702@gmail.com>
2016-09-06 07:27:26 +0900

    minor fix

--- common/layers.py
+++ common/layers.py
@@ -214,14 +214,14 @@ class Convolution:
     def forward(self, x):
         FN, C, FH, FW = self.W.shape
         N, C, H, W = x.shape
-        out_h = 1 + (H + 2*self.pad - FH) / self.stride
-        out_w = 1 + (W + 2*self.pad - FW) / self.stride
+        out_h = 1 + int((H + 2*self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2*self.pad - FW) / self.stride)
 
         col = im2col(x, FH, FW, self.stride, self.pad)
         col_W = self.W.reshape(FN, -1).T
 
         out = np.dot(col, col_W) + self.b
-        out = out.reshape(N, int(out_h), int(out_w), -1).transpose(0,3,1,2)
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
 
         self.x = x
         self.col = col
