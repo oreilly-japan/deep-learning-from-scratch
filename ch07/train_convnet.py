c19fa3be47827934ae41b3666773c8c76690388c
kt <kannnotgt@gmail.com>
2016-10-25 22:20:55 +0900

    fix undefined bug

--- ch07/train_convnet.py
+++ ch07/train_convnet.py
@@ -14,12 +14,14 @@ from common.trainer import Trainer
 #x_train, t_train = x_train[:5000], t_train[:5000]
 #x_test, t_test = x_test[:1000], t_test[:1000]
 
+max_epochs = 20
+
 network = SimpleConvNet(input_dim=(1,28,28), 
                         conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
                         hidden_size=100, output_size=10, weight_init_std=0.01)
                         
 trainer = Trainer(network, x_train, t_train, x_test, t_test,
-                  epochs=20, mini_batch_size=100,
+                  max_epochs, mini_batch_size=100,
                   optimizer='Adam', optimizer_param={'lr': 0.001},
                   evaluate_sample_num_per_epoch=1000)
 trainer.train()
@@ -31,8 +33,8 @@ print("Saved Network Parameters!")
 # グラフの描画
 markers = {'train': 'o', 'test': 's'}
 x = np.arange(max_epochs)
-plt.plot(x, train_acc_list, marker='o', label='train', markevery=2)
-plt.plot(x, test_acc_list, marker='s', label='test', markevery=2)
+plt.plot(x, trainer.train_acc_list, marker='o', label='train', markevery=2)
+plt.plot(x, trainer.test_acc_list, marker='s', label='test', markevery=2)
 plt.xlabel("epochs")
 plt.ylabel("accuracy")
 plt.ylim(0, 1.0)
