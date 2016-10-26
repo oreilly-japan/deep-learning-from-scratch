315e08f42f07ca9a099dac723c09aa13eaa7e7b6
Saitoh Koki <koki0702@gmail.com>
2016-10-27 07:37:18 +0900

    Minor fix

--- ch07/train_convnet.py
+++ ch07/train_convnet.py
@@ -14,14 +14,12 @@ from common.trainer import Trainer
 #x_train, t_train = x_train[:5000], t_train[:5000]
 #x_test, t_test = x_test[:1000], t_test[:1000]
 
-max_epochs = 20
-
 network = SimpleConvNet(input_dim=(1,28,28), 
                         conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
                         hidden_size=100, output_size=10, weight_init_std=0.01)
                         
 trainer = Trainer(network, x_train, t_train, x_test, t_test,
-                  max_epochs, mini_batch_size=100,
+                  epochs=20, mini_batch_size=100,
                   optimizer='Adam', optimizer_param={'lr': 0.001},
                   evaluate_sample_num_per_epoch=1000)
 trainer.train()
