661776902b387081936904441a37a62ac83fd0ae
yue82 <no.82.vuf.lab@gmail.com>
2016-11-02 23:55:43 +0900

    fix dimentional error of gradient_2d

--- ch04/gradient_2d.py
+++ ch04/gradient_2d.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     X = X.flatten()
     Y = Y.flatten()
     
-    grad = numerical_gradient(function_2, np.array([X, Y]) )
+    grad = numerical_gradient(function_2, np.array([X, Y]).T).T
     
     plt.figure()
     plt.quiver(X, Y, -grad[0], -grad[1],  angles="xy",color="#666666")#,headwidth=10,scale=40,color="#444444")
