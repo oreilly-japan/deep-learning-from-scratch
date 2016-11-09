544af4086f7079e77bde060b9edc1f8a1378dbed
Koki Saitoh <koki0702@gmail.com>
2016-11-10 07:47:15 +0900

    Fix undefined variable

--- common/multi_layer_net.py
+++ common/multi_layer_net.py
@@ -97,7 +97,7 @@ class MultiLayerNet:
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        if t.ndim != 1 : T = np.argmax(t, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
