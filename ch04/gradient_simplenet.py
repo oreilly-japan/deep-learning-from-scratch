911528e2a3ea912e4605f4c2ddfaf39d56c16764
Koki Saitoh <koki0702@gmail.com>
2016-11-10 08:37:41 +0900

    Minor fix : rand -> randn

--- ch04/gradient_simplenet.py
+++ ch04/gradient_simplenet.py
@@ -8,7 +8,7 @@ from common.gradient import numerical_gradient
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.rand(2,3)
+        self.W = np.random.randn(2,3)
 
     def predict(self, x):
         return np.dot(x, self.W)
