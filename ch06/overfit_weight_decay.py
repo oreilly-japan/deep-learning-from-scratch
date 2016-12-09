0c573a874991911a3ddfc0323d12c903826a480a
Wegra Lee <wegra.lee@gmail.com>
2016-12-10 04:38:30 +0900

    주석 번역

--- ch06/overfit_weight_decay.py
+++ ch06/overfit_weight_decay.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
@@ -11,18 +11,18 @@ from common.optimizer import SGD
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
 
-# 過学習を再現するために、学習データを削減
+# 오버피팅을 재현하기 위해 학습 데이터 수를 줄임
 x_train = x_train[:300]
 t_train = t_train[:300]
 
-# weight decay（荷重減衰）の設定 =======================
-#weight_decay_lambda = 0 # weight decayを使用しない場合
+# weight decay（가중치 감쇠） 설정 =======================
+#weight_decay_lambda = 0 # weight decay를 사용하지 않을 경우
 weight_decay_lambda = 0.1
 # ====================================================
 
 network = MultiLayerNet(input_size=784, hidden_size_list=[100, 100, 100, 100, 100, 100], output_size=10,
                         weight_decay_lambda=weight_decay_lambda)
-optimizer = SGD(lr=0.01)
+optimizer = SGD(lr=0.01) # 학습률이 0.01인 SGD로 매개변수 갱신
 
 max_epochs = 201
 train_size = x_train.shape[0]
@@ -56,7 +56,7 @@ for i in range(1000000000):
             break
 
 
-# 3.グラフの描画==========
+# 3.그래프 그리기==========
 markers = {'train': 'o', 'test': 's'}
 x = np.arange(max_epochs)
 plt.plot(x, train_acc_list, marker='o', label='train', markevery=10)
