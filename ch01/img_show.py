a5e654ccbf2ab1f3b5ee5a4dd973717e6af69d3c
Wegra Lee <wegra.lee@gmail.com>
2016-12-09 22:50:12 +0900

    주석 번역

--- ch01/img_show.py
+++ ch01/img_show.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 
-img = imread('../dataset/lena.png') # 画像の読み込み
+img = imread('../dataset/lena.png') # 이미지 읽어오기
 plt.imshow(img)
 
 plt.show()
