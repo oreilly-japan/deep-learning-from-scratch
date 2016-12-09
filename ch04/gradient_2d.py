c1903167fcb6311af6405565f7dfbc3b05d1947e
Wegra Lee <wegra.lee@gmail.com>
2016-12-10 02:29:11 +0900

    주석 보강

--- ch04/gradient_2d.py
+++ ch04/gradient_2d.py
@@ -7,18 +7,21 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def _numerical_gradient_no_batch(f, x):
     h = 1e-4 # 0.0001
-    grad = np.zeros_like(x)
+    grad = np.zeros_like(x) # x와 형상이 같은 배열을 생성
     
     for idx in range(x.size):
         tmp_val = x[idx]
+        
+        # f(x+h) 계산
         x[idx] = float(tmp_val) + h
-        fxh1 = f(x) # f(x+h)
+        fxh1 = f(x)
         
+        # f(x-h) 계산
         x[idx] = tmp_val - h 
-        fxh2 = f(x) # f(x-h)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
+        fxh2 = f(x) 
         
-        x[idx] = tmp_val # 원래 값으로 되돌린다.
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 값 복원
         
     return grad
 
