ea8e7d2b756c027651e29e86828d8ec573ec7980
Wegra Lee <wegra.lee@gmail.com>
2016-12-09 22:49:10 +0900

    주석 번역

--- ch01/sin_cos_graph.py
+++ ch01/sin_cos_graph.py
@@ -2,16 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# データの作成
-x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
+# 데이터 준비
+x = np.arange(0, 6, 0.1) # 0에서 6까지 0.1 간격으로 생성
 y1 = np.sin(x)
 y2 = np.cos(x)
 
-# グラフの描画
+# 그래프 그리기
 plt.plot(x, y1, label="sin")
-plt.plot(x, y2, linestyle = "--", label="cos")
-plt.xlabel("x") # x軸のラベル
-plt.ylabel("y") # y軸のラベル
+plt.plot(x, y2, linestyle = "--", label="cos") # cos 함수는 점선으로 그리기
+plt.xlabel("x") # x축 이름
+plt.ylabel("y") # y축 이름
 plt.title('sin & cos')
 plt.legend()
 plt.show()
