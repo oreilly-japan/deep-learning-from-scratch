ce5251754f59fdb45eff8fd8ffac7a4c40809ae0
Wegra Lee <wegra.lee@gmail.com>
2016-12-09 22:46:30 +0900

    주석 번역 & 범위를 (0, 7, 0.1)에서 (0, 6, 0.1)로 변경

--- ch01/sin_graph.py
+++ ch01/sin_graph.py
@@ -2,10 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# データの作成
-x = np.arange(0, 7, 0.1)
+# 데이터 준비
+x = np.arange(0, 6, 0.1) # 0에서 6까지 0.1 간격으로 생성
 y = np.sin(x)
 
-# グラフの描画
+# 그래프 그리기
 plt.plot(x, y)
 plt.show()
