ecce3036b8f159ae99f53aeb9815de612f376da0
Wegra Lee <wegra.lee@gmail.com>
2016-12-09 23:51:10 +0900

    주석 번역

--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 import pickle
 from dataset.mnist import load_mnist
