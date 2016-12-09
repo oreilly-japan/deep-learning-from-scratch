e3378545ca46b79186ec60941ef3b7de9b15d659
Wegra Lee <wegra.lee@gmail.com>
2016-12-10 01:00:08 +0900

    주석 번역

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
@@ -17,7 +17,7 @@ label = t_train[0]
 print(label)  # 5
 
 print(img.shape)  # (784,)
-img = img.reshape(28, 28)  # 形状を元の画像サイズに変形
+img = img.reshape(28, 28)  # 형상을 원래 이미지의 크기로 변형
 print(img.shape)  # (28, 28)
 
 img_show(img)
