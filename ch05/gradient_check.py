d3682e4d5d0fb7d8c3ccb4173144332c31e6e031
Wegra Lee <wegra.lee@gmail.com>
2016-12-10 03:38:49 +0900

    주석 번역 및 보강

--- ch05/gradient_check.py
+++ ch05/gradient_check.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 
-# データの読み込み
+# 데이터 읽기
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
@@ -16,6 +16,7 @@ t_batch = t_train[:3]
 grad_numerical = network.numerical_gradient(x_batch, t_batch)
 grad_backprop = network.gradient(x_batch, t_batch)
 
+# 각 가중치의 절대 오차의 평균을 구한다.
 for key in grad_numerical.keys():
     diff = np.average( np.abs(grad_backprop[key] - grad_numerical[key]) )
     print(key + ":" + str(diff))
