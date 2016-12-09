10db0d2f813487ab599abaab62f0a76b66d0d1c3
Wegra Lee <wegra.lee@gmail.com>
2016-12-10 04:29:12 +0900

    주석 번역

--- ch06/weight_init_activation_histogram.py
+++ ch06/weight_init_activation_histogram.py
@@ -14,10 +14,10 @@ def ReLU(x):
 def tanh(x):
     return np.tanh(x)
     
-input_data = np.random.randn(1000, 100)  # 1000個のデータ
-node_num = 100  # 各隠れ層のノード（ニューロン）の数
-hidden_layer_size = 5  # 隠れ層が5層
-activations = {}  # ここにアクティベーションの結果を格納する
+input_data = np.random.randn(1000, 100)  # 1000개의 데이터
+node_num = 100  # 각 은닉층의 노드(뉴런) 수
+hidden_layer_size = 5  # 은닉층이 5개
+activations = {}  # 이곳에 활성화 결과를 저장
 
 x = input_data
 
@@ -25,7 +25,7 @@ for i in range(hidden_layer_size):
     if i != 0:
         x = activations[i-1]
 
-    # 初期値の値をいろいろ変えて実験しよう！
+    # 초깃값을 다양하게 바꿔가며 실험해보자！
     w = np.random.randn(node_num, node_num) * 1
     # w = np.random.randn(node_num, node_num) * 0.01
     # w = np.random.randn(node_num, node_num) * np.sqrt(1.0 / node_num)
@@ -35,14 +35,14 @@ for i in range(hidden_layer_size):
     a = np.dot(x, w)
 
 
-    # 活性化関数の種類も変えて実験しよう！
+    # 활성화 함수도 바꿔가며 실험해보자！
     z = sigmoid(a)
     # z = ReLU(a)
     # z = tanh(a)
 
     activations[i] = z
 
-# ヒストグラムを描画
+# 히스토그램 그리기
 for i, a in activations.items():
     plt.subplot(1, len(activations), i+1)
     plt.title(str(i+1) + "-layer")
