38826835d43c30cc292d2767112e950b32731038
Wegra Lee <wegra.lee@gmail.com>
2016-12-09 23:52:08 +0900

    주석 번역

--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -37,7 +37,7 @@ network = init_network()
 accuracy_cnt = 0
 for i in range(len(x)):
     y = predict(network, x[i])
-    p= np.argmax(y) # 最も確率の高い要素のインデックスを取得
+    p= np.argmax(y) # 확률이 가장 높은 원소의 인덱스를 얻는다.
     if p == t[i]:
         accuracy_cnt += 1
 
