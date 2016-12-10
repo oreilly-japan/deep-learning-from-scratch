08a6a4df98191839c5d2f7e609846959442db542
Wegra Lee <wegra.lee@gmail.com>
2016-12-10 22:47:34 +0900

    주석 번역

--- ch06/weight_init_compare.py
+++ ch06/weight_init_compare.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
@@ -11,7 +11,7 @@ from common.multi_layer_net import MultiLayerNet
 from common.optimizer import SGD
 
 
-# 0:MNISTデータの読み込み==========
+# 0. MNIST 데이터 읽기==========
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
 
 train_size = x_train.shape[0]
@@ -19,7 +19,7 @@ batch_size = 128
 max_iterations = 2000
 
 
-# 1:実験の設定==========
+# 1. 실험용 설정==========
 weight_init_types = {'std=0.01': 0.01, 'Xavier': 'sigmoid', 'He': 'relu'}
 optimizer = SGD(lr=0.01)
 
@@ -31,7 +31,7 @@ for key, weight_type in weight_init_types.items():
     train_loss[key] = []
 
 
-# 2:訓練の開始==========
+# 2. 훈련 시작==========
 for i in range(max_iterations):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
@@ -51,7 +51,7 @@ for i in range(max_iterations):
             print(key + ":" + str(loss))
 
 
-# 3.グラフの描画==========
+# 3. 그래프 그리기==========
 markers = {'std=0.01': 'o', 'Xavier': 's', 'He': 'D'}
 x = np.arange(max_iterations)
 for key in weight_init_types.keys():
