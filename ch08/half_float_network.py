2842dfbfbc52b98dcbebb9c625e622c1c8644d6a
Wegra Lee <wegra.lee@gmail.com>
2016-12-10 23:16:55 +0900

    주석 번역

--- ch08/half_float_network.py
+++ ch08/half_float_network.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 import matplotlib.pyplot as plt
 from deep_convnet import DeepConvNet
@@ -12,14 +12,14 @@ from dataset.mnist import load_mnist
 network = DeepConvNet()
 network.load_params("deep_convnet_params.pkl")
 
-sampled = 10000 # 高速化のため
+sampled = 10000 # 고속화를 위한 표본추출
 x_test = x_test[:sampled]
 t_test = t_test[:sampled]
 
 print("caluculate accuracy (float64) ... ")
 print(network.accuracy(x_test, t_test))
 
-# float16に型変換
+# float16(반정밀도)로 형변환
 x_test = x_test.astype(np.float16)
 for param in network.params.values():
     param[...] = param.astype(np.float16)
