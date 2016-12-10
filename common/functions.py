145efeb6dfd12484ca12ad9908e650402f8b6aae
Wegra Lee <wegra.lee@gmail.com>
2016-12-10 23:19:59 +0900

    주석 번역

--- common/functions.py
+++ common/functions.py
@@ -35,7 +35,7 @@ def softmax(x):
         y = np.exp(x) / np.sum(np.exp(x), axis=0)
         return y.T 
 
-    x = x - np.max(x) # オーバーフロー対策
+    x = x - np.max(x) # 오버플로 대책
     return np.exp(x) / np.sum(np.exp(x))
 
 
@@ -48,7 +48,7 @@ def cross_entropy_error(y, t):
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
         
-    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
     if t.size == y.size:
         t = t.argmax(axis=1)
              
