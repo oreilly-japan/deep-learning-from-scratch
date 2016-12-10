9cdbe9f542d50909301608ec150383dd772759a8
Wegra Lee <wegra.lee@gmail.com>
2016-12-10 22:37:03 +0900

    주석 번역

--- ch06/batch_norm_gradient_check.py
+++ ch06/batch_norm_gradient_check.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 from dataset.mnist import load_mnist
 from common.multi_layer_net_extend import MultiLayerNetExtend
 
-# データの読み込み
+# 데이터 읽기
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 network = MultiLayerNetExtend(input_size=784, hidden_size_list=[100, 100], output_size=10,
