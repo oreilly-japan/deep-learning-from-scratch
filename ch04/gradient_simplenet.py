c8a360be3747d2e022c77cfed36b18a7cca886ba
Wegra Lee <wegra.lee@gmail.com>
2016-12-22 17:42:59 +0900

    주석 수정

--- ch04/gradient_simplenet.py
+++ ch04/gradient_simplenet.py
@@ -8,7 +8,7 @@ from common.gradient import numerical_gradient
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.randn(2,3) # 가우스 분포로 초기화
+        self.W = np.random.randn(2,3) # 정규분포로 초기화
 
     def predict(self, x):
         return np.dot(x, self.W)
