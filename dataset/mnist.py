d93b53ce35a085d645bbf946a06e95242162823c
Akira Takahashi <faithandbrave@gmail.com>
2017-02-28 18:59:49 +0900

    remove unnecessary end-of-line space

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -29,46 +29,46 @@ img_size = 784
 
 def _download(file_name):
     file_path = dataset_dir + "/" + file_name
-    
+
     if os.path.exists(file_path):
         return
 
     print("Downloading " + file_name + " ... ")
     urllib.request.urlretrieve(url_base + file_name, file_path)
     print("Done")
-    
+
 def download_mnist():
     for v in key_file.values():
        _download(v)
-        
+
 def _load_label(file_name):
     file_path = dataset_dir + "/" + file_name
-    
+
     print("Converting " + file_name + " to NumPy Array ...")
     with gzip.open(file_path, 'rb') as f:
             labels = np.frombuffer(f.read(), np.uint8, offset=8)
     print("Done")
-    
+
     return labels
 
 def _load_img(file_name):
     file_path = dataset_dir + "/" + file_name
-    
-    print("Converting " + file_name + " to NumPy Array ...")    
+
+    print("Converting " + file_name + " to NumPy Array ...")
     with gzip.open(file_path, 'rb') as f:
             data = np.frombuffer(f.read(), np.uint8, offset=16)
     data = data.reshape(-1, img_size)
     print("Done")
-    
+
     return data
-    
+
 def _convert_numpy():
     dataset = {}
     dataset['train_img'] =  _load_img(key_file['train_img'])
-    dataset['train_label'] = _load_label(key_file['train_label'])    
+    dataset['train_label'] = _load_label(key_file['train_label'])
     dataset['test_img'] = _load_img(key_file['test_img'])
     dataset['test_label'] = _load_label(key_file['test_label'])
-    
+
     return dataset
 
 def init_mnist():
@@ -83,45 +83,45 @@ def _change_ont_hot_label(X):
     T = np.zeros((X.size, 10))
     for idx, row in enumerate(T):
         row[X[idx]] = 1
-        
+
     return T
-    
+
 
 def load_mnist(normalize=True, flatten=True, one_hot_label=False):
     """MNISTデータセットの読み込み
-    
+
     Parameters
     ----------
     normalize : 画像のピクセル値を0.0~1.0に正規化する
-    one_hot_label : 
+    one_hot_label :
         one_hot_labelがTrueの場合、ラベルはone-hot配列として返す
         one-hot配列とは、たとえば[0,0,1,0,0,0,0,0,0,0]のような配列
-    flatten : 画像を一次元配列に平にするかどうか 
-    
+    flatten : 画像を一次元配列に平にするかどうか
+
     Returns
     -------
     (訓練画像, 訓練ラベル), (テスト画像, テストラベル)
     """
     if not os.path.exists(save_file):
         init_mnist()
-        
+
     with open(save_file, 'rb') as f:
         dataset = pickle.load(f)
-    
+
     if normalize:
         for key in ('train_img', 'test_img'):
             dataset[key] = dataset[key].astype(np.float32)
             dataset[key] /= 255.0
-            
+
     if one_hot_label:
         dataset['train_label'] = _change_ont_hot_label(dataset['train_label'])
-        dataset['test_label'] = _change_ont_hot_label(dataset['test_label'])    
-    
+        dataset['test_label'] = _change_ont_hot_label(dataset['test_label'])
+
     if not flatten:
          for key in ('train_img', 'test_img'):
             dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
 
-    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label']) 
+    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
 
 
 if __name__ == '__main__':
