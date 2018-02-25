6ee503380ecfadfefc2d5333c96aa0b41a1ffc4f
unknown <ishizuka.hdk@gmail.com>
2018-02-25 16:18:56 +0900

    試し

--- ch01/img_show.py
+++ ch01/img_show.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__))) # どこでも実行できるようにする
 
 img = imread('../dataset/lena.png') # 画像の読み込み
 plt.imshow(img)
