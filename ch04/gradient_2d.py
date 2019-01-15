fix dimension of argument for numerical_gradient()

ch04/gradient_2d.py
@@ -56,9 +56,9 @@ if __name__ == '__main__':
     
     X = X.flatten()
     Y = Y.flatten()
-    
-    grad = numerical_gradient(function_2, np.array([X, Y]))
-    
+
+    grad = numerical_gradient(function_2, np.array([X, Y]).T).T
+
     plt.figure()
     plt.quiver(X, Y, -grad[0], -grad[1],  angles="xy",color="#666666")
     plt.xlim([-2, 2])