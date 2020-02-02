446aa0859843d4f9e5c7c4659ade149958806b0c
Wegra Lee <wegra.lee@gmail.com>
2020-02-02 21:41:29 +0900

    이미지 교체

--- ch01/img_show.py
+++ ch01/img_show.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 
-img = imread('../dataset/.png') # 이미지 읽어오기
+img = imread('../dataset/cactus.png') # 이미지 읽어오기
 plt.imshow(img)
 
 plt.show()
