d6df7fd9a116f6ee1f6bfac27648b0ca41c933a9
nizyu <en19926@gmail.com>
2020-10-06 03:58:18 +0900

    改修

--- ch05/train_neuralnet.py
+++ ch05/train_neuralnet.py
@@ -4,12 +4,20 @@ sys.path.append(os.pardir)
 
 import numpy as np
 from dataset.mnist import load_mnist
-from two_layer_net import TwoLayerNet
+from two_layer_net import Network
+from layers import LayerDifinition
 
 # データの読み込み
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+network = Network([
+    LayerDifinition('affine', {'input': 784, 'output': 100, 'weight_init_std': 0.01}),
+    LayerDifinition('relu', {}),
+    LayerDifinition('affine', {'input': 100, 'output': 20, 'weight_init_std': 0.01}),
+    LayerDifinition('relu', {}),
+    LayerDifinition('affine', {'input': 20, 'output': 10, 'weight_init_std': 0.01}),
+    LayerDifinition('softmax_with_loss', {}),
+])
 
 iters_num = 10000
 train_size = x_train.shape[0]
@@ -26,18 +34,12 @@ for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    
-    # 勾配
-    #grad = network.numerical_gradient(x_batch, t_batch)
-    grad = network.gradient(x_batch, t_batch)
-    
-    # 更新
-    for key in ('W1', 'b1', 'W2', 'b2'):
-        network.params[key] -= learning_rate * grad[key]
-    
+
+    network.learning(x_batch, t_batch, learning_rate)
+
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
-    
+
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
--- common/layers.py
+++ common/layers.py
@@ -1,10 +1,31 @@
 # coding: utf-8
+import sys
 import numpy as np
 from common.functions import *
 from common.util import im2col, col2im
 
+# factory
+class LayerDifinition:
+    def __init__(self, name, options):
+        self.name = name
+        self.options = options
+
+    def create(self):
+        if self.name == Relu.name:
+            return Relu()
+        elif self.name == Sigmoid.name:
+            return Sigmoid()
+        elif self.name == Affine.name:
+            return Affine(self.options['input'], self.options['output'], self.options['weight_init_std'])
+        elif self.name == SoftmaxWithLoss.name:
+            return SoftmaxWithLoss()
+        else:
+            print("unknown layer")
+            sys.exit()
 
 class Relu:
+    name = 'relu'
+
     def __init__(self):
         self.mask = None
 
@@ -21,8 +42,13 @@ class Relu:
 
         return dx
 
+    def learning(self, learning_rate):
+        pass
+
 
 class Sigmoid:
+    name = 'sigmoid'
+
     def __init__(self):
         self.out = None
 
@@ -36,12 +62,16 @@ class Sigmoid:
 
         return dx
 
+    def learning(self, learning_rate):
+        pass
 
 class Affine:
-    def __init__(self, W, b):
-        self.W =W
-        self.b = b
-        
+    name = 'affine'
+
+    def __init__(self, input, output, weight_init_std):
+        self.W = weight_init_std * np.random.randn(input, output)
+        self.b = np.zeros(output)
+
         self.x = None
         self.original_x_shape = None
         # 重み・バイアスパラメータの微分
@@ -62,12 +92,17 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
-        
+
         dx = dx.reshape(*self.original_x_shape)  # 入力データの形状に戻す（テンソル対応）
         return dx
 
+    def learning(self, learning_rate):
+        self.W -= learning_rate * self.dW
+        self.b -= learning_rate * self.db
 
 class SoftmaxWithLoss:
+    name = 'softmax_with_loss'
+
     def __init__(self):
         self.loss = None
         self.y = None # softmaxの出力
@@ -77,7 +112,7 @@ class SoftmaxWithLoss:
         self.t = t
         self.y = softmax(x)
         self.loss = cross_entropy_error(self.y, self.t)
-        
+
         return self.loss
 
     def backward(self, dout=1):
@@ -88,197 +123,8 @@ class SoftmaxWithLoss:
             dx = self.y.copy()
             dx[np.arange(batch_size), self.t] -= 1
             dx = dx / batch_size
-        
-        return dx
-
-
-class Dropout:
-    """
-    http://arxiv.org/abs/1207.0580
-    """
-    def __init__(self, dropout_ratio=0.5):
-        self.dropout_ratio = dropout_ratio
-        self.mask = None
-
-    def forward(self, x, train_flg=True):
-        if train_flg:
-            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
-            return x * self.mask
-        else:
-            return x * (1.0 - self.dropout_ratio)
-
-    def backward(self, dout):
-        return dout * self.mask
-
-
-class BatchNormalization:
-    """
-    http://arxiv.org/abs/1502.03167
-    """
-    def __init__(self, gamma, beta, momentum=0.9, running_mean=None, running_var=None):
-        self.gamma = gamma
-        self.beta = beta
-        self.momentum = momentum
-        self.input_shape = None # Conv層の場合は4次元、全結合層の場合は2次元  
-
-        # テスト時に使用する平均と分散
-        self.running_mean = running_mean
-        self.running_var = running_var  
-        
-        # backward時に使用する中間データ
-        self.batch_size = None
-        self.xc = None
-        self.std = None
-        self.dgamma = None
-        self.dbeta = None
-
-    def forward(self, x, train_flg=True):
-        self.input_shape = x.shape
-        if x.ndim != 2:
-            N, C, H, W = x.shape
-            x = x.reshape(N, -1)
-
-        out = self.__forward(x, train_flg)
-        
-        return out.reshape(*self.input_shape)
-            
-    def __forward(self, x, train_flg):
-        if self.running_mean is None:
-            N, D = x.shape
-            self.running_mean = np.zeros(D)
-            self.running_var = np.zeros(D)
-                        
-        if train_flg:
-            mu = x.mean(axis=0)
-            xc = x - mu
-            var = np.mean(xc**2, axis=0)
-            std = np.sqrt(var + 10e-7)
-            xn = xc / std
-            
-            self.batch_size = x.shape[0]
-            self.xc = xc
-            self.xn = xn
-            self.std = std
-            self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * mu
-            self.running_var = self.momentum * self.running_var + (1-self.momentum) * var            
-        else:
-            xc = x - self.running_mean
-            xn = xc / ((np.sqrt(self.running_var + 10e-7)))
-            
-        out = self.gamma * xn + self.beta 
-        return out
-
-    def backward(self, dout):
-        if dout.ndim != 2:
-            N, C, H, W = dout.shape
-            dout = dout.reshape(N, -1)
-
-        dx = self.__backward(dout)
-
-        dx = dx.reshape(*self.input_shape)
-        return dx
-
-    def __backward(self, dout):
-        dbeta = dout.sum(axis=0)
-        dgamma = np.sum(self.xn * dout, axis=0)
-        dxn = self.gamma * dout
-        dxc = dxn / self.std
-        dstd = -np.sum((dxn * self.xc) / (self.std * self.std), axis=0)
-        dvar = 0.5 * dstd / self.std
-        dxc += (2.0 / self.batch_size) * self.xc * dvar
-        dmu = np.sum(dxc, axis=0)
-        dx = dxc - dmu / self.batch_size
-        
-        self.dgamma = dgamma
-        self.dbeta = dbeta
-        
-        return dx
-
-
-class Convolution:
-    def __init__(self, W, b, stride=1, pad=0):
-        self.W = W
-        self.b = b
-        self.stride = stride
-        self.pad = pad
-        
-        # 中間データ（backward時に使用）
-        self.x = None   
-        self.col = None
-        self.col_W = None
-        
-        # 重み・バイアスパラメータの勾配
-        self.dW = None
-        self.db = None
-
-    def forward(self, x):
-        FN, C, FH, FW = self.W.shape
-        N, C, H, W = x.shape
-        out_h = 1 + int((H + 2*self.pad - FH) / self.stride)
-        out_w = 1 + int((W + 2*self.pad - FW) / self.stride)
-
-        col = im2col(x, FH, FW, self.stride, self.pad)
-        col_W = self.W.reshape(FN, -1).T
-
-        out = np.dot(col, col_W) + self.b
-        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
-
-        self.x = x
-        self.col = col
-        self.col_W = col_W
-
-        return out
-
-    def backward(self, dout):
-        FN, C, FH, FW = self.W.shape
-        dout = dout.transpose(0,2,3,1).reshape(-1, FN)
-
-        self.db = np.sum(dout, axis=0)
-        self.dW = np.dot(self.col.T, dout)
-        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
-
-        dcol = np.dot(dout, self.col_W.T)
-        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
 
         return dx
 
-
-class Pooling:
-    def __init__(self, pool_h, pool_w, stride=2, pad=0):
-        self.pool_h = pool_h
-        self.pool_w = pool_w
-        self.stride = stride
-        self.pad = pad
-        
-        self.x = None
-        self.arg_max = None
-
-    def forward(self, x):
-        N, C, H, W = x.shape
-        out_h = int(1 + (H - self.pool_h) / self.stride)
-        out_w = int(1 + (W - self.pool_w) / self.stride)
-
-        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
-        col = col.reshape(-1, self.pool_h*self.pool_w)
-
-        arg_max = np.argmax(col, axis=1)
-        out = np.max(col, axis=1)
-        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
-
-        self.x = x
-        self.arg_max = arg_max
-
-        return out
-
-    def backward(self, dout):
-        dout = dout.transpose(0, 2, 3, 1)
-        
-        pool_size = self.pool_h * self.pool_w
-        dmax = np.zeros((dout.size, pool_size))
-        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
-        dmax = dmax.reshape(dout.shape + (pool_size,)) 
-        
-        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
-        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
-        
-        return dx
+    def learning(self, learning_rate):
+        pass
