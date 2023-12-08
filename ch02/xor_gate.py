e18543c44610f4bfc9cf20234ccf2a97271d2605
pirotyyy <hiroki1423@docomo.ne.jp>
2023-12-08 11:07:07 +0900

    feat: done

--- ch02/xor_gate.py
+++ ch02/xor_gate.py
@@ -2,6 +2,7 @@
 from and_gate import AND
 from or_gate import OR
 from nand_gate import NAND
+import matplotlib.pyplot as plt
 
 
 def XOR(x1, x2):
@@ -10,7 +11,24 @@ def XOR(x1, x2):
     y = AND(s1, s2)
     return y
 
-if __name__ == '__main__':
-    for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
-        y = XOR(xs[0], xs[1])
-        print(str(xs) + " -> " + str(y))
+
+if __name__ == "__main__":
+    ys = []
+    xs = [(0, 0), (1, 0), (0, 1), (1, 1)]
+    for x in xs:
+        y = XOR(x[0], x[1])
+        ys.append(y)
+        print(str(x) + " -> " + str(y))
+
+    # Plotting
+    for i, point in enumerate(xs):
+        if ys[i] == 0:
+            plt.scatter(point[0], point[1], marker="*", s=100)  # Star for 0
+        else:
+            plt.scatter(point[0], point[1], marker="o", s=100)  # Circle for 1
+
+    plt.xlabel("Input 1")
+    plt.ylabel("Input 2")
+    plt.title("XOR Gate Output")
+    plt.grid(True)
+    plt.savefig("xor.png")
