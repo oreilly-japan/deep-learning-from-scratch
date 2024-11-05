07b11618cdb5d5f78ad45464de0730efc07b0a4a
koki0702 <koki0702@gmail.com>
2024-11-05 15:55:06 +0900

    fix np.int -> int

--- common/functions.py
+++ common/functions.py
@@ -7,16 +7,16 @@ def identity_function(x):
 
 
 def step_function(x):
-    return np.array(x > 0, dtype=np.int)
+    return np.array(x > 0, dtype=int)
 
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))    
+    return 1 / (1 + np.exp(-x))
 
 
 def sigmoid_grad(x):
     return (1.0 - sigmoid(x)) * sigmoid(x)
-    
+
 
 def relu(x):
     return np.maximum(0, x)
@@ -26,7 +26,7 @@ def relu_grad(x):
     grad = np.zeros_like(x)
     grad[x>=0] = 1
     return grad
-    
+
 
 def softmax(x):
     x = x - np.max(x, axis=-1, keepdims=True)   # オーバーフロー対策
@@ -41,11 +41,11 @@ def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
-        
+
     # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
     if t.size == y.size:
         t = t.argmax(axis=1)
-             
+
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
